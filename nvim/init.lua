80a613d456d2e7d931f8e301e721955638454684
Mark Brudnak <malibrud@gmail.com>
2025-12-13 10:48:50 -0500

    initial

---
+++ nvim/init.lua
@@ -0,0 +1,31 @@
+require("core.options")
+require("core.keymaps")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+require("lazy").setup({
+	require("plugins.alpha"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformatting"),
+	require("plugins.bufferline"),
+	require("plugins.colortheme"),
+	require("plugins.comment"),
+	require("plugins.gitsigns"),
+	require("plugins.indent-blankline"),
+	require("plugins.lsp"),
+	require("plugins.lualine"),
+	require("plugins.misc"),
+	require("plugins.neotree"),
+	require("plugins.telescope"),
+	require("plugins.treesitter"),
+})
