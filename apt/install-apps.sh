eb27ede1d27fd8cae9163eb7f173b33c9d71e4c2
Mark Brudnak <malibrud@gmail.com>
2025-12-28 09:42:44 -0500

    apt installers

---
+++ apt/install-apps.sh
@@ -0,0 +1,24 @@
+sudo apt update && sudo apt upgrade
+
+sudo apt install -y \
+    binutils \
+    build-essential \
+    clang \
+    curl \
+    neovim \
+    luarocks \
+    gdb \
+    git \
+    libclang-dev \
+    lld \
+    lldb \
+    llvm \
+    llvm-dev \
+    odin \
+    python3 \
+    python3-pip \
+    python3-venv \
+    ripgrep \
+    tmux \
+    tree-sitter-cli \
+    wget
